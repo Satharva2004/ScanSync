2a8face6b02b4c2fe4adb50931a5d9f55e6be68e
Atharva Sawant <84018291+Satharva2004@users.noreply.github.com>
2023-10-17 09:09:06 +0000

    Add existing project files to Git

---
+++ qr - Copy.js
@@ -0,0 +1,7 @@
+let imgBox = document.getElementById("imgBox");
+let qrImg = document.getElementById("qrImg");
+let qrText = document.getElementById("qrText");
+function generateQR() {
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
+}
